f4d3eaf5da92321be453099e7a3875aaa7dc9d20
ritucs <rituparnocs@gmail.com>
2024-09-05 21:34:47 +0530

    Update

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,5 +2,8 @@
 
 import com.exmaple.Docker
 def call(String imageName){
+      script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+      script.sh "echo '${script.PASS}' | docker login -u ${script.USER} --password-stdin"
+    }
      return new Docker(this).dockerPush(imageName)
 }
